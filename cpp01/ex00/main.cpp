db7a1c019555e729865b8dc6b497da09fc13a019
M-elhouary <mohamedelhouary056@gmail.com>
2026-01-06 18:17:32 +0100

    ex00 is finished <allocation> && run time &&  fix comment

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -3,18 +3,15 @@
 
 
 
-// Prototypes for the allocation functions
 Zombie* newZombie(std::string name);
 void    randomChump(std::string name);
 
 int main() {
-    // Stack: destroyed automatically when randomChump finishes
     randomChump("Stack_Zombie");
 
-    // Heap: persistent until you manually delete it
     Zombie* heapZombie = newZombie("Heap_Zombie");
     heapZombie->announce();
-    delete heapZombie; // This triggers the destructor! [cite: 137]
+    delete heapZombie;
 
     return 0;
 }
