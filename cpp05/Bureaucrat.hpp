8e579f7d25f4413bbec881677559c2d0c0db8f2c
Mohamed El-Houary <mel-houa@c2r7p10.1337.ma>
2026-04-17 22:29:40 +0100

    ex00

---
+++ cpp05/Bureaucrat.hpp
@@ -0,0 +1,17 @@
+#ifndef Bureaucrat_HPP
+#define Bureaucrat_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+class Bureaucrat {
+
+    private:
+        std::string  const name;
+    public:
+    
+
+};
+
+#endif
