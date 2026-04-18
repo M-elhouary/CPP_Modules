5297916f64ef5b6511a9b518cf2201a5cfa42727
Mohamed El-Houary <mel-houa@c2r6p2.1337.ma>
2026-04-18 18:20:30 +0100

    add header file for ex00

--- cpp05/Bureaucrat.hpp
+++ cpp05/Bureaucrat.hpp
@@ -3,14 +3,31 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <string>
 
 class Bureaucrat {
 
     private:
         std::string  const name;
+        int grade;
     public:
-    
+        Bureaucrat();
+        std::string const &getName() const;
+        int getGrade() const;
+        Bureaucrat(const std::string& n, int g);
+        Bureaucrat(Bureaucrat const &other);
+        Bureaucrat &operator=(Bureaucrat const &other);
+        void incrementGrade();
+        void decrementGrade();
+        ~Bureaucrat();
+
+        class GradeTooHighException  : public std::exception {
+            public :
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception {
+            public :
+                virtual const char* what() const throw();
+        };
 
 };
 
